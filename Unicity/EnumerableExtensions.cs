ab24680c356a7cbd527a2f16e1f48795860aa296
Mathieu Moreault <seranx@gmail.com>
2023-04-27 20:48:28 -0400

    Using generic MaxValue

--- Unicity/EnumerableExtensions.cs
+++ Unicity/EnumerableExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class EnumerableExtensions
 {
-    public static TNumber GetNextAvailableIdOrDefault<TNumber>(this IEnumerable<IAutoIncrementedId<TNumber>> source, TNumber defaultValue = default) where TNumber : struct, INumber<TNumber>
+    public static TNumber GetNextAvailableIdOrDefault<TNumber>(this IEnumerable<IAutoIncrementedId<TNumber>> source, TNumber defaultValue = default) where TNumber : struct, INumber<TNumber>, IMinMaxValue<TNumber>
     {
         return source.GetNextAvailableNumberOrDefault(x => x.Id, defaultValue);
     }
@@ -13,17 +13,14 @@ public static class EnumerableExtensions
         return source.GroupBy(x => x.Id).Where(x => x.Count() > 1).Select(x => x.Key).Any();
     }
 
-    public static TNumber GetNextAvailableNumberOrDefault<TSource, TNumber>(this IEnumerable<TSource> source, Func<TSource, TNumber?> selector, TNumber defaultValue) where TNumber : struct, INumber<TNumber>
+    public static TNumber GetNextAvailableNumberOrDefault<TSource, TNumber>(this IEnumerable<TSource> source, Func<TSource, TNumber?> selector, TNumber defaultValue) where TNumber : struct, INumber<TNumber>, IMinMaxValue<TNumber>
     {
         if (source == null) throw new ArgumentNullException(nameof(source));
         if (selector == null) throw new ArgumentNullException(nameof(selector));
 
         var maxId = source.Max(selector);
-        return maxId switch
-        {
-            null => defaultValue,
-            int.MaxValue => throw new Exception(string.Format(Exceptions.CannotIncrementBecauseMaxValue, nameof(Int32), int.MaxValue)),
-            _ => maxId.Value + TNumber.One
-        };
+        if (maxId == null) return defaultValue;
+        if (maxId.Value == TNumber.MaxValue) throw new Exception(string.Format(Exceptions.CannotIncrementBecauseMaxValue, typeof(TNumber).Name, TNumber.MaxValue));
+        return maxId.Value + TNumber.One;
     }
 }
