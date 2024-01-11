54f04a8ad633f75c08a1e1c13532051f7a5be7d9
Mathieu Moreault <seranx@gmail.com>
2024-01-11 16:43:19 -0500

    Added a new exception type

--- Unicity/EnumerableExtensions.cs
+++ Unicity/EnumerableExtensions.cs
@@ -2,20 +2,39 @@
 
 public static class EnumerableExtensions
 {
-    [Obsolete("Use GetNextAvailableIdOrDefault instead. Will be removed in 2.2.0")]
+    /// <summary>
+    /// Returns the next available id for the given source.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="NumberIncrementationException{TNumber}"/>
+    [Obsolete("Use GetNextAvailableIdOrDefault instead. Will be removed in 3.0.0")]
     public static int GetNextAvailableId(this IEnumerable<IAutoIncrementedId<int>> source) => source.GetNextAvailableIdOrDefault();
 
+    /// <summary>
+    /// Returns the next available id for the given source.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="NumberIncrementationException{TNumber}"/>
     public static TNumber GetNextAvailableIdOrDefault<TNumber>(this IEnumerable<IAutoIncrementedId<TNumber>> source, TNumber defaultValue = default) where TNumber : struct, INumber<TNumber>, IMinMaxValue<TNumber>
     {
         return source.GetNextAvailableNumberOrDefault(x => x.Id, defaultValue);
     }
 
+    /// <summary>
+    /// Returns true if the given source contains duplicate ids.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"/>
     public static bool ContainsDuplicateIds<TNumber>(this IEnumerable<IAutoIncrementedId<TNumber>> source) where TNumber : struct, INumber<TNumber>
     {
         if (source == null) throw new ArgumentNullException(nameof(source));
         return source.GroupBy(x => x.Id).Where(x => x.Count() > 1).Select(x => x.Key).Any();
     }
 
+    /// <summary>
+    /// Returns the next available number for the given source and selector.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="NumberIncrementationException{TNumber}"/>
     public static TNumber GetNextAvailableNumberOrDefault<TSource, TNumber>(this IEnumerable<TSource> source, Func<TSource, TNumber?> selector, TNumber defaultValue = default) where TNumber : struct, INumber<TNumber>, IMinMaxValue<TNumber>
     {
         if (source == null) throw new ArgumentNullException(nameof(source));
@@ -23,7 +42,7 @@ public static class EnumerableExtensions
 
         var maxId = source.Max(selector);
         if (maxId == null) return defaultValue;
-        if (maxId.Value == TNumber.MaxValue) throw new Exception(string.Format(Exceptions.CannotIncrementBecauseMaxValue, typeof(TNumber).Name, TNumber.MaxValue));
+        if (maxId.Value == TNumber.MaxValue) throw new NumberIncrementationException<TNumber>();
         return maxId.Value + TNumber.One;
     }
 }
