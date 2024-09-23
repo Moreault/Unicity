a7ddd9491d346ad55ddd0e23f0ac6a6a25c4e170
Mathieu Moreault <seranx@gmail.com>
2024-09-23 08:59:27 -0400

    Updated nugets, removed obsolete method

--- Unicity.Tests/GlobalUsings.cs
+++ Unicity.Tests/GlobalUsings.cs
@@ -6,7 +6,6 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using ToolBX.Dummies;
-global using ToolBX.Eloquentest.Dummies;
 global using ToolBX.Unicity;
 global using ToolBX.Unicity.Resources;
 global using WhiteJackalStudio.TestTools;
